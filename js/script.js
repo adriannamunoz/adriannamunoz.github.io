82981fa7ae8aa2ffaf750c52d9b9c2ed91159cba
adriannamunoz <105665083+adriannamunoz@users.noreply.github.com>
2022-07-20 12:01:06 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 // To top button
 let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction () {
+if (
+     document.body.scrollTop > 20 ||
+     document.documentElement.scrollTop > 20
+ ) {
+     mybutton.style.display = "block";
+ } else {
+     mybutton.style.display = "none";
+   }
+ }
+ // When clicked, scroll to top of document
+ mybutton.addEventListener("click", backToTop);
+
+ function backToTop() {
+    document.body.scollTop = 0;
+    document.documentElement.scrollTop = 0;
+ }
